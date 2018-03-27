fffce9785d9d6012bff020d97e6e4d7c23d49d41
Tadeu Grigolon <degrigo@gmail.com>
2018-03-27 20:33:20 -0300

    developed Pedido Test

--- 502/aula02/Pedido/Pedido.php
+++ 502/aula02/Pedido/Pedido.php
@@ -1,2 +1,32 @@
 <?php
 
+require '../Estoque/Estoque.php';
+
+class Pedido
+{
+	private $item;
+	private $quantidade;
+	private $finalizado = false;
+
+	public function __construct(string $item, int $quantidade)
+	{
+		$this->item = $item;
+		$this->quantidade = $quantidade;
+	}
+
+	// TypeHint Estoque (precisa ser da classe estoque)
+	public function fechar(Estoque $estoque)
+	{
+		// Indo no estoque, verificando se a quantidade é maior ou igual do que eu tenho em estoque
+		if ($estoque->get($this->item) >= $this->quantidade) {
+			$this->finalizado = true;
+			$estoque->remove($this->item, $this->quantidade);
+		}
+	}
+
+	public function isFinalizado()
+	{
+		return $this->finalizado;
+	}
+
+}
--- 502/aula02/Pedido/PedidoTest.php
+++ 502/aula02/Pedido/PedidoTest.php
@@ -6,7 +6,24 @@ use PHPUnit\Framework\TestCase;
 
 class PedidoTest extends TestCase
 {
+	private $estoque;
 
+	// Criando estoque e adicionando dois itens com estoques diferentes
+	public function setUp()
+	{
+		$this->estoque = new Estoque();
+		$this->estoque->add('Blusa X', 10);
+		$this->estoque->add('Blusa Y', 8);
+	}
+
+	public function testDeveFecharPedido()
+	{
+		$pedido = new Pedido('Blusa X', 4);
+		$pedido->fechar($this->estoque);
+
+		$this->assertTrue($pedido->isFinalizado());
+		$this->assertSame(6 , $this->estoque->get('Blusa X'));
+	}
 
 
 }
