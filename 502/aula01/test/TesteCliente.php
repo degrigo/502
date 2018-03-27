0507dba319e99b8510bc7252b6c3b28af24e70f1
Tadeu Grigolon <degrigo@gmail.com>
2018-03-26 21:24:01 -0300

    developed more functions to assert on PHP Unit Test

--- 502/aula01/test/TesteCliente.php
+++ 502/aula01/test/TesteCliente.php
@@ -9,8 +9,10 @@ class ClienteTest extends TestCase
 
 	public function testNovoCliente()
 	{
-		$cliente = new Cliente("Tadeu Grigolon");
+		$cliente = new Cliente("Tadeu Grigolon", 34, 'degrigo@gmail.com');
 		$this->assertSame("Tadeu Grigolon", $cliente->getNome());
+		$this->assertSame(34, $cliente->getIdade());
+		$this->assertSame('degrigo@gmail.com', $cliente->getEmail());
 	}
 
 }
