788d9810d3cd5cc549d6ec575aa26fa0e84ca328
Tadeu Grigolon <degrigo@gmail.com>
2018-03-27 22:27:21 -0300

    Working with mktime, strtotime

---
+++ 502/aula02/dateHour/strtotime.php
@@ -0,0 +1,29 @@
+<?php
+
+echo strtotime('now');
+
+echo "<hr>";
+
+echo strtotime('27 March 2018');
+
+echo "<hr>";
+
+echo strtotime('+1 day');
+
+echo "<hr>";
+
+echo strtotime('+1 week');
+
+echo "<hr>";
+
+echo strtotime('+1 day 2 days 4 hours 2 seconds');
+
+echo "<hr>";
+
+echo strtotime('next Wednesday');
+
+echo "<hr>";
+
+echo strtotime('last Sunday');
+
+echo "<hr>";
