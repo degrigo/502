f125a0e165bd33940cd6d8a6a68491cd97b1b2c7
Tadeu Grigolon <degrigo@gmail.com>
2018-03-27 21:30:34 -0300

    Developed testNaoDeveFecharPedido

--- 502/aula02/Pedido/PedidoTest.php
+++ 502/aula02/Pedido/PedidoTest.php
@@ -11,18 +11,53 @@ class PedidoTest extends TestCase
 	// Criando estoque e adicionando dois itens com estoques diferentes
 	public function setUp()
 	{
-		$this->estoque = new Estoque();
-		$this->estoque->add('Blusa X', 10);
-		$this->estoque->add('Blusa Y', 8);
+		$this->estoque = $this->getMockBuilder('Estoque')->getMock();
 	}
 
 	public function testDeveFecharPedido()
 	{
-		$pedido = new Pedido('Blusa X', 4);
+		$item = "Blusa X";
+		$quantidade = 10;
+
+		$this->estoque->expects($this->once())
+				->method("get")
+				// with - parametros que eu quero passar
+				->with($this->equalTo($item))
+				// will - parametros a serem retornados
+				->will($this->returnValue($quantidade));
+
+		$this->estoque->expects($this->once())
+				->method("remove")
+				->with(
+						$this->equalTo($item),
+						$this->equalTo($quantidade)
+					);
+
+		$pedido = new Pedido($item, $quantidade);
 		$pedido->fechar($this->estoque);
 
 		$this->assertTrue($pedido->isFinalizado());
-		$this->assertSame(6 , $this->estoque->get('Blusa X'));
+	}
+
+	public function testNaoDeveFecharPedido()
+	{
+		$item = "Blusa Y";
+		$quantidade = 7;
+		// se retornar 0, nunca executar o método remove do estoque
+		$this->estoque->expects($this->once())
+				->method("get")
+				->with($this->equalTo($item))
+				->will($this->returnValue(0));
+
+		$this->estoque->expects($this->never())
+				->method("remove");
+
+		$pedido = new Pedido($item, $quantidade);
+		$pedido->fechar($this->estoque);
+
+		// Verificando se o pedido está retornando false por não estar finalizado.
+		$this->assertFalse($pedido->isFinalizado());
+
 	}
 
 
